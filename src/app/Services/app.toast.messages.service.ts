bd42edc9a20afb095a365e944aa683227bc15d8f
AmilaViduranga <amila@info-share.org>
2017-07-15 00:55:53 +0530

    add message service to handle all the toast messaging callings

---
+++ src/app/Services/app.toast.messages.service.ts
@@ -0,0 +1,50 @@
+/**
+ * Created by Amila on 7/15/2017.
+ * this is the common place that handle all the message displaying thing
+ * <simple-notifications></<simple-notifications> tag should be on consumer page
+ * for more details follow this link
+ * https://github.com/flauc/angular2-notifications/blob/master/docs/toastNotifications.md
+ */
+import { NotificationsService } from 'angular2-notifications';
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class ToastMessagesService {
+  constructor(private toastMessagesService: NotificationsService) {
+
+  }
+  /*
+   * successfull message
+   */
+  successMessage(title, message) {
+    return this.toastMessagesService.success(
+      title,
+      message,
+      {
+        timeOut: 5000,
+        showProgressBar: true,
+        pauseOnHover: false,
+        clickToClose: false,
+        maxLength: 100
+      }
+    );
+  }
+
+  /*
+   * error message
+   */
+  errorMessage(title, message) {
+    return this.toastMessagesService.error(
+      title,
+      message,
+      {
+        timeOut: 5000,
+        showProgressBar: true,
+        pauseOnHover: false,
+        clickToClose: false,
+        maxLength: 100
+      }
+    );
+  }
+
+}
